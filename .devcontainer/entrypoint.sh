51eb094a452344fc37521102fb54ec741d6a95d7
Michel Hidalgo <mhidalgo@theaiinstitute.com>
2024-12-03 09:57:06 -0300

    Update CI and dev workflows
    Signed-off-by: Michel Hidalgo <mhidalgo@theaiinstitute.com>

--- .devcontainer/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd /spot-cpp-sdk/cpp
-if [ -d build ]; then
-  rm -r build
-fi
-mkdir build
-cd build
-cmake .. -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=TRUE -DBUILD_CHOREOGRAPHY_LIBS=ON -DBUILD_SHARED_LIBS=ON
-make -j8 install package
